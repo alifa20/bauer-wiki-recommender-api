d14e6431c87415dfc183672374693edf9ce0d13a
Ali Fathieh <ali.fathieh@gmail.com>
2015-11-26 20:58:13 +1100

    Access-Control-Allow-Origin

--- p.py
+++ p.py
@@ -6,15 +6,26 @@ import yaml
 import datetime
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/test', 'test'
 )
 
 class index:
+    def OPTIONS(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Access-Control-Allow-Headers', 'Content-Disposition, Content-Type, Packaging, Authorization')
+        web.header('Access-Control-Allow-Method', '*')
+        return
+
     def GET(self):
+        web.header('Access-Control-Allow-Origin',      '*')
+        web.header('Access-Control-Allow-Credentials', 'true')
         return "Wiki recommender api. <br /> %s" % datetime.datetime.now().strftime("%H:%M:%S.%f")
 
     def POST(self):
         # i = web.input()
+        web.header('Access-Control-Allow-Origin',      '*')
+        web.header('Access-Control-Allow-Credentials', 'true')
         data = web.data()
         jdata = yaml.safe_load(data)
         people = graphlab.SFrame('people_wiki.gl/')
@@ -32,10 +43,23 @@ class index:
         for i in xrange(1,5):
             p = people[people['name']==query_result[i]["reference_label"]]
             arr_res.append({'name': p["name"][0], 'uri': p["URI"][0]})
-
         web.header('Content-Type', 'application/json')
+        
         return json.dumps(arr_res)
 
+class test:
+    def OPTIONS(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Access-Control-Allow-Headers', 'Content-Disposition, Content-Type, Packaging, Authorization')
+        web.header('Access-Control-Allow-Method', '*')
+        return
+    def GET(self):
+        web.header('Access-Control-Allow-Origin',      '*')
+        web.header('Access-Control-Allow-Credentials', 'true')
+        return "this is test %s" % datetime.datetime.now().strftime("%H:%M:%S.%f")    
+    def POST(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        return 'test, %s' % datetime.datetime.now().strftime("%H:%M:%S.%f")
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
